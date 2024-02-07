856b5f7f1089319681d6153f20df75228cd8ad96
roderick <roderick_lu@163.com>
2024-02-07 14:31:21 +0800

    fix: remove useless files

--- src/configs/common.ts
+++ src/configs/common.ts
@@ -12,29 +12,6 @@ export const isProd = import.meta.env.MODE === 'production';
 
 export const isDev = import.meta.env.MODE === 'development';
 
-export const medias = [
-  {
-    icon: getImageUrl('@/assets/images/medias/twitter.svg'),
-    link: 'https://twitter.com/SubstanceX_',
-    label: 'Twitter',
-  },
-  {
-    icon: getImageUrl('@/assets/images/medias/discord.svg'),
-    link: 'https://discord.gg/substancex',
-    label: 'Discord',
-  },
-  {
-    icon: getImageUrl('@/assets/images/medias/telegram.svg'),
-    link: 'https://t.me/Substancexofficial',
-    label: 'Telegram',
-  },
-  {
-    icon: getImageUrl('@/assets/images/medias/medium.svg'),
-    link: 'https://medium.com/@SubstanceX',
-    label: 'Medium',
-  },
-];
-
 export const {
   // goerli
   VITE_APP_LOCK_CONTRACT,
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import NProgress from '@/components/_global/NProgress';
 import App from './App.tsx';
+import '@/assets/styles/reset.css';
 import 'virtual:uno.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
