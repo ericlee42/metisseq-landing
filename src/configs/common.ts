96eed8c5f6ea758c553dfeb8365dcb1ad2c38f37
ericlee <eric.l@metis.io>
2024-04-11 00:07:21 +0800

    Revert "version 0.0.3 subgraph"
    This reverts commit de39ca47d107c41f776e3278e2f071cab3dda834.

--- src/configs/common.ts
+++ src/configs/common.ts
@@ -99,9 +99,9 @@ export const setL2Provider = (rpcUrl, chainId) => {
 
 export const graphUrl = {
   staking: {
-    [mainnet.id.toString()]: 'https://subgraph.satsuma-prod.com/b91d46e5018b/metis/metis-sequencer-locking/version/v0.0.3/api',
+    [mainnet.id.toString()]: 'https://subgraph.satsuma-prod.com/b91d46e5018b/metis/metis-sequencer-locking/api',
     [holesky.id.toString()]: 'https://graphnode.holesky.metisdevops.link/subgraphs/name/metisio/sequencer-locking',
-    [sepolia.id.toString()]: 'https://subgraph.satsuma-prod.com/metis/metis-sepolia-sequencer-locking/version/v0.0.3/api',
+    [sepolia.id.toString()]: 'https://subgraph.satsuma-prod.com/b91d46e5018b/metis/metis-sepolia-sequencer-locking/api',
   },
   block: {
     [mainnet.id.toString()]: 'https://andromeda-subgraph.metisdevops.link/subgraphs/name/metisio/sequencer-set',
