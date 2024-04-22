0951dad9b250964f78b71edd3b5729db3776cb41
Aiden <aiden.a@metis.io>
2024-04-23 00:37:54 +0800

    joinedDuration chain id not get, default same as mainnet

--- src/pages/SequencerDetail/index.tsx
+++ src/pages/SequencerDetail/index.tsx
@@ -521,7 +521,7 @@ export function Component() {
     // desc，todo make sure sort without error
     let fromDate = fetchUserTxData?.histories?.filter((i) => i.action === 'Lock')?.[0]?.timestamp;
     const genesisSignersMainnet = ['0xeca7ae7de0d1978df299a547ee66c4503fba474d', '0xa233cc81fc6c12e3318ea71ec5d7bba78c706b04', '0xaff606251d8540f97ca2db12774c0147a170ab9e'];
-    if (chainId && chainId === 1 && genesisSignersMainnet.indexOf(id!.toLowerCase()) >= 0) {
+    if ((!chainId || chainId === 1) && genesisSignersMainnet.indexOf(id!.toLowerCase()) >= 0) {
       fromDate = 1710406800; // 14/03/2024 9:00:00 UTC
     }
 
