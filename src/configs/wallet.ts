425ff2fff7811c9d4b42c4fed1a7442b984023ee
Harry-Chen-Dev <harrychendev@gmail.com>
2024-11-08 19:25:01 +0800

    fix: modify wagmi configuration for mainnet and spelia/holesky

--- src/configs/wallet.ts
+++ src/configs/wallet.ts
@@ -1,10 +1,101 @@
-import { publicProvider } from '@wagmi/core/providers/public';
 import { InjectedConnector } from '@wagmi/core';
-import { WagmiConfig, configureChains, createConfig, mainnet } from 'wagmi';
+import { publicProvider } from '@wagmi/core/providers/public';
 import { createPublicClient, defineChain, http } from 'viem';
+import { WagmiConfig, configureChains, createConfig } from 'wagmi';
 
 import { isProd } from './common';
-import { holesky, sepolia } from 'viem/chains';
+
+export const _mainnet = defineChain({
+  id: 1,
+  network: 'homestead',
+  name: 'Ethereum',
+  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    alchemy: {
+      http: ['https://eth-mainnet.g.alchemy.com/v2'],
+      webSocket: ['wss://eth-mainnet.g.alchemy.com/v2'],
+    },
+    infura: {
+      http: ['https://mainnet.infura.io/v3'],
+      webSocket: ['wss://mainnet.infura.io/ws/v3'],
+    },
+    default: {
+      http: ['https://eth-mainnet.token.im'],
+    },
+    public: {
+      http: ['https://eth-mainnet.token.im'],
+    },
+  },
+  blockExplorers: {
+    etherscan: {
+      name: 'Etherscan',
+      url: 'https://etherscan.io',
+    },
+    default: {
+      name: 'Etherscan',
+      url: 'https://etherscan.io',
+    },
+  },
+  contracts: {
+    ensRegistry: {
+      address: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+    },
+    ensUniversalResolver: {
+      address: '0xc0497E381f536Be9ce14B0dD3817cBcAe57d2F62',
+      blockCreated: 16966585,
+    },
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 14353601,
+    },
+  },
+});
+
+
+export const _sepolia = defineChain({
+  id: 11_155_111,
+  network: 'sepolia',
+  name: 'Sepolia',
+  nativeCurrency: { name: 'Sepolia Ether', symbol: 'SEP', decimals: 18 },
+  rpcUrls: {
+    alchemy: {
+      http: ['https://eth-sepolia.g.alchemy.com/v2'],
+      webSocket: ['wss://eth-sepolia.g.alchemy.com/v2'],
+    },
+    infura: {
+      http: ['https://sepolia.infura.io/v3'],
+      webSocket: ['wss://sepolia.infura.io/ws/v3'],
+    },
+    default: {
+      http: ['https://lb.drpc.org/ogrpc?network=ethereum&dkey=ArAUTX6RD0lbvBRkMslCxKwPuC6CnaoR76UfFhW5UfFk'],
+    },
+    public: {
+      http: ['https://lb.drpc.org/ogrpc?network=ethereum&dkey=ArAUTX6RD0lbvBRkMslCxKwPuC6CnaoR76UfFhW5UfFk'],
+    },
+  },
+  blockExplorers: {
+    etherscan: {
+      name: 'Etherscan',
+      url: 'https://sepolia.etherscan.io',
+    },
+    default: {
+      name: 'Etherscan',
+      url: 'https://sepolia.etherscan.io',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 751532,
+    },
+    ensRegistry: { address: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e' },
+    ensUniversalResolver: {
+      address: '0x21B000Fd62a880b2125A61e36a284BB757b76025',
+      blockCreated: 3914906,
+    },
+  },
+  testnet: true,
+});
 
 // disable wss, which is not available
 const _holesky = defineChain({
@@ -29,7 +120,7 @@ const _holesky = defineChain({
   testnet: true,
 });
 
-export const network = isProd ? [mainnet, sepolia] : [mainnet, sepolia, _holesky];
+export const network = isProd ? [_mainnet, _sepolia] : [_mainnet, _sepolia, _holesky];
 
 export const injectedConnector = new InjectedConnector({
   chains: [...network],
@@ -44,26 +135,27 @@ const config = createConfig({
 });
 
 export const mainnetTxPublicClient = createPublicClient({
-  chain: mainnet,
+  chain: _mainnet,
   // transport,
   transport: http(),
 });
 export const holeskyTxPublicClient = createPublicClient({
-  chain: holesky,
+  chain: _holesky,
   // transport,
   transport: http(),
 });
 
 export const sepoliaTxPublicClient = createPublicClient({
-  chain: sepolia,
+  chain: _sepolia,
   // transport,
   transport: http(),
 });
 
 export const txPublicClients = {
-  [sepolia.id.toString()]: sepoliaTxPublicClient,
-  [mainnet.id.toString()]: mainnetTxPublicClient,
-  [holesky.id.toString()]: holeskyTxPublicClient,
+  [_sepolia.id.toString()]: sepoliaTxPublicClient,
+  [_mainnet.id.toString()]: mainnetTxPublicClient,
+  [_holesky.id.toString()]: holeskyTxPublicClient,
 };
 
-export { config, WagmiConfig as WagmiProvider, publicClient };
+export { WagmiConfig as WagmiProvider, config, publicClient };
+
